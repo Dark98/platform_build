omnirom/android_build

build: refactor setting CUSTOM_BUILD
there is no real need to set this in check_product
do it once afterwards it is clear what  will be

Change-Id: I586bb3f94248f596cb0454d1a0e1a2a2a8f64a24

envsetup.sh
@@ -129,14 +129,6 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-
-    if (echo -n $1 | grep -q -e "^omni_") ; then
-       CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^omni_//g')
-    else
-       CUSTOM_BUILD=
-    fi
-    export CUSTOM_BUILD
-
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -674,6 +666,13 @@ function lunch()
         echo
         return 1
     fi
+
+    if (echo -n $product | grep -q -e "^omni_") ; then
+       CUSTOM_BUILD=$(echo -n $product | sed -e 's/^omni_//g')
+    else
+       CUSTOM_BUILD=
+    fi
+    export CUSTOM_BUILD
 
     export TARGET_PRODUCT=$product
     export TARGET_BUILD_VARIANT=$variant

Dark98/platform_build

envsetup.sh
@@ -131,14 +131,6 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-
-    if (echo -n $1 | grep -q -e "^custom_") ; then
-       CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^custom_//g')
-    else
-       CUSTOM_BUILD=
-    fi
-    export CUSTOM_BUILD
-
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -643,6 +635,13 @@ function lunch()
       unset TARGET_PLATFORM_VERSION
     fi
     export TARGET_BUILD_TYPE=release
+
+    if (echo -n $product | grep -q -e "^choose_") ; then
+       CUSTOM_BUILD=$(echo -n $product | sed -e 's/^choose_//g')
+    else
+       CUSTOM_BUILD=
+    fi
+    export CUSTOM_BUILD
 
     echo
 